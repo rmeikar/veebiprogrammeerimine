991a7701385f41c3e173d00939332bf791643579
Rain Meikar <rmeikar@users.noreply.github.com>
2017-09-11 10:36:57 +0300

    HTML koodile lisatud esimene php osa

---
+++ esimene.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Rain Meikari veebiprogremise asjad</title>
+</head>
+<body>
+	<h1>Rain Meikar</h1>
+	<p>See veebileht on loodud veebiprogrammeerimise kursusel ning ei sisalda mingisugust tõsiseltvõetavat sisu.</p>
+	<p>See tekst on kirjutatud kodus täiesti iseseisvalt</p>
+	<p>See on juurdekirjutis</p>
+	
+	<?php
+		echo "Täna on vastik ilm!";
+	?>
+</body>
+</html>
